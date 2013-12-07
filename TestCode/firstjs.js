b52426d207ba28dc5ce8fc9279bde509c90ac438
Ashish Prasad <ashishpd@hotmail.com>
2013-12-07 13:03:04 -0800

    working js with more parsing. also curl files for full and small login

--- TestCode/firstjs.js
+++ TestCode/firstjs.js
@@ -13,7 +13,26 @@ fs.readFile(filename, 'utf8', function(err, data) {
   var pos2 = data.indexOf('"', pos1 + search.length);
   var viewState = data.substr(pos1, pos2-pos1);
 
+  search = "id=\"__EVENTVALIDATION\" value=\"";
+  pos1 = data.indexOf(search);
+  pos2 = data.indexOf('"', pos1 + search.length);
+  var eventValidation = data.substr(pos1, pos2-pos1);
+
+  var userNameField = data.match("name=\"(.+?\\$txtUsername)\"")[1];
+  var passwordField = data.match("name=\"(.+?\\$txtPassword)\"")[1];
+  //var scriptManagerField = data.match("'ScriptManager', 'Form', \\['(.+?)'")[1];
+  var webFormField = data.match("WebForm_PostBackOptions\\(&quot;(.+?\\$cmdLogin)&quot;")[1];
+  //var loginField = data.match("id=\"(.+?_Login_UP)\"")[1];
+
+
+
   console.log(viewState);
+  console.log(eventValidation);
+  console.log(userNameField);
+  console.log(passwordField);
+  //console.log(scriptManagerField);
+  console.log(webFormField);
+  //console.log(loginField);
 
   console.log(data.length);
   console.log(pos1);
