64531609eb014f0568ed94c6d54be7cbc046e8c8
Ashish Prasad <ashishpd@hotmail.com>
2014-03-20 22:22:41 -0700

    nice looking handheld and table answers list view

--- DnnMobi/app/controllers/answers.js
+++ DnnMobi/app/controllers/answers.js
@@ -102,11 +102,11 @@ var args = arguments[0] || {};
 		contentSummary:'summary', 
 		questionVotes: 22, 
 		totalAnswers: 33},
-		{contentTitle: 'title', 
+		{contentTitle: 'Playing video games well can get you into a top South Korean university', 
 		lastActiveRelativeDate: '5 minutes ago', 
-		contentSummary:'summary', 
+		contentSummary:'this is a long summary line. There are a plethora of geo-location-based apps that make it incredibly convenient to do friendly things, like chat with nearby peers about local hotspots or meet up with a coworker on the fly. A new iOS app called Cloak, however, utilizes services from Foursquare and Instagram for a more anti-social purpose. The brainchild of Brian Moore and former Buzzfeed director creative director Chris Baker, Cloak identifies the location of friends (read: those youd rather not bump into) based upon their latest check-in. While perusing the map, you can choose to "flag" certain undesirables, like exes or annoying third-wheels, to be notified when they wander within a preset distance of your personal bubble. Or you could, ya know, skip town altogether just to be safe.', 
 		questionVotes: 5, 
-		totalAnswers: 6}]
+		totalAnswers: 7}]
 	};	
 	dummyData.responseText = JSON.stringify(json);
 	
